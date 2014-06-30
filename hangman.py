7bee8966696ad3b4c3458cc7e51c5e94b7743f20
ravitezu <ravi-teja@live.com>
2014-06-30 22:27:34 +0530

    Tweaks

--- hangman.py
+++ hangman.py
@@ -1,15 +1,7 @@
 #!/usr/bin/python
-# 6.00 Problem Set 3
-# 
-# Hangman game
-#
-
 # -----------------------------------
-# Helper code
-# You don't need to understand this helper code,
-# but you will have to know how to use the functions
-# (so be sure to read the docstrings!)
-
+# Hangman game
+# -----------------------------------
 import random
 import string
 
