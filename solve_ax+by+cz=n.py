660e3d53649aa2ede594ffae36e3f6079fbf24e5
RaviTeja <rpothan@corp.untd.com>
2014-07-14 11:37:46 +0530

    Adding ax+by+cz=n.py

--- solve_ax+by+cz=n.py
+++ solve_ax+by+cz=n.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python
 
 def McNuggets(n):
-    """ 6a+9b+20c = n"""
+    """ This snippet can be used for finding the values of x,y,z in ax+by+cz=n where a,b,c and n are known."""
     if n == 0:
         return True
     for i in (6, 9, 20):
