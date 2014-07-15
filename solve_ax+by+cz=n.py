4e3e6d12637cf1d283827f267dc621962e5d8c55
ravitezu <ravi-teja@live.com>
2014-07-15 17:26:24 +0530

    Tweaks

--- solve_ax+by+cz=n.py
+++ solve_ax+by+cz=n.py
@@ -1,10 +1,13 @@
 #!/usr/bin/python
 
-def McNuggets(n):
+def runMe(n):
     """ This snippet can be used for finding the values of x,y,z in ax+by+cz=n where a,b,c and n are known."""
     if n == 0:
         return True
     for i in (6, 9, 20):
-        if n >= i and McNuggets(n - i):
+        if n >= i and runMe(n - i):
             return True
     return False 
+
+if __name__ == "__main__":
+    print runMe(241)
