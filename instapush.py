0bc11a2318db0c0713baeb4f4f6c88481366f619
RaviTezu <ravi-teja@live.com>
2014-11-19 23:00:03 +0530

    more

--- instapush.py
+++ instapush.py
@@ -1,35 +1,44 @@
 #!/usr/bin/python
+import sys
 import pycurl, json
 
-appID = "xxxxxxxxxxxxxxxxxx"
-appSecret = "xxxxxxxxxxxxxxxxxxx"
+appID = "xxxxxxxxxxxxxxxxxxxxxxx"
+appSecret = "xxxxxxxxxxxxxxxxxxxxx"
 pushEvent = "Status"
-pushMessage = "is going up.."
 
-#use Curl to post the Instapush API
-c = pycurl.Curl()
+def main():
+    #use Curl to post the Instapush API
+    c = pycurl.Curl()
 
-#set Instapush API URL
-c.setopt(c.URL, 'https://api.instapush.im/v1/post')
+    #set Instapush API URL
+    c.setopt(c.URL, 'https://api.instapush.im/v1/post')
 
-# setup custom headers for authentication variables and content type
-c.setopt(c.HTTPHEADER, ['x-instapush-appid: ' + appID, 'x-instapush-appsecret: ' + appSecret, 'Content-Type: application/json'])
+    #setup custom headers for authentication variables and content type
+    c.setopt(c.HTTPHEADER, ['x-instapush-appid: ' + appID,
+                            'x-instapush-appsecret: ' + appSecret, 
+                            'Content-Type: application/json'])
 
-# create a dictionary structure for the JSON data to post to Instapush
-json_fields = {}
+    #create a dictionary structure for the JSON data to post to Instapush
+    json_fields = {}
 
-# setup JSON values
-json_fields['event']=pushEvent
-json_fields['trackers'] = {}
-json_fields['trackers']['message']=pushMessage
+    #setup JSON values
+    json_fields['event']=pushEvent
+    json_fields['trackers'] = {}
+    json_fields['trackers']['message']=pushMessage
+    postfields = json.dumps(json_fields)
 
-postfields = json.dumps(json_fields)
+    #make sure to send the JSON with post
+    c.setopt(c.POSTFIELDS, postfields)
 
-# make sure to send the JSON with post
-c.setopt(c.POSTFIELDS, postfields)
+    #send push notification
+    c.perform()
 
-#send push notification
-c.perform()
+    #cleanup
+    c.close()
 
-#cleanup
-c.close()
+if __name__ == "__main__" and len(sys.argv)>1:
+    pushMessage = str(sys.argv[1])
+    main()
+else: 
+    print "instapush.py <Message>"
+    sys.exit(1)
